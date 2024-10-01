61adaafe1284162c06aae1602b06d8622f054a1b
Naveen-Raj-M <naveenrajmanoharan@gmail.com>
2024-10-01 14:05:48 -0500

    added exercise for course 2 week 2

---
+++ handwritten_digit_recognition/handwritten_digit_recognition.py
@@ -0,0 +1,130 @@
+'''
+Multi-class classification model to recognize the handwritten digits 0 to 9
+'''
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.activations import linear, relu, sigmoid
+import matplotlib.pyplot as plt
+import logging
+import warnings
+
+def display_errors(model, X, y):
+    f = model.predict(X)
+    yhat = np.argmax(f, axis=1)
+    indexes = np.where(yhat != y[:,0])[0]
+
+    if len(indexes) == 0:
+        print("no errors found")
+    else:
+        count = min(8, len(indexes))
+        fig, ax = plt.subplots(1, count, figsize=(5, 1.2))
+        fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.80])
+
+        for i in range(count):
+            j = indexes[i]
+            X_reshaped = X[j].reshape((20,20)).T
+            ax[i].imshow(X_reshaped, cmap='gray')
+
+            prediction = model.predict(X[j].reshape(1,400))
+            prediction_p = tf.nn.softmax(prediction)
+            yhat = np.argmax(prediction_p)
+
+            ax[i].set_title(f"{y[j,0], yhat}", fontsize=10)
+            ax[i].set_axis_off()
+        fig.suptitle("Label, yhat", fontsize=12)
+        plt.show()
+    return(len(indexes))
+
+
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+X = np.load("./c2_w1/X.npy")
+y = np.load("./c2_w1/y.npy")
+
+print ('The first element of y is: ', y[0,0])
+print ('The last element of y is: ', y[-1,0])
+
+print ('The shape of X is: ' + str(X.shape))
+print ('The shape of y is: ' + str(y.shape))
+
+tf.random.set_seed(1234)
+model = Sequential(
+    [
+        tf.keras.layers.InputLayer((400,)),
+        tf.keras.layers.Dense(25, activation="relu", name="L1"),
+        tf.keras.layers.Dense(15, activation="relu", name="L2"),
+        tf.keras.layers.Dense(10, activation="linear", name="L3")
+    ], name = "digit_recognition" 
+)
+model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+)
+
+model.summary()
+
+[layer1, layer2, layer3] = model.layers
+
+W1,b1 = layer1.get_weights()
+W2,b2 = layer2.get_weights()
+W3,b3 = layer3.get_weights()
+print(f"W1 shape = {W1.shape}, b1 shape = {b1.shape}")
+print(f"W2 shape = {W2.shape}, b2 shape = {b2.shape}")
+print(f"W3 shape = {W3.shape}, b3 shape = {b3.shape}")
+
+history = model.fit(X, y, epochs=40) 
+
+image_of_two = X[1015]
+
+prediction = model.predict(image_of_two.reshape(1,400))
+
+print(f" predicting a Two: \n{prediction}")
+print(f" Largest Prediction index: {np.argmax(prediction)}")
+
+prediction_p = tf.nn.softmax(prediction)
+
+print(f" predicting a Two. Probability vector: \n{prediction_p}")
+print(f"Total of predictions: {np.sum(prediction_p):0.3f}")
+
+yhat = np.argmax(prediction_p)
+
+print(f"np.argmax(prediction_p): {yhat}")
+
+plt.figure(figsize=(8, 6))
+plt.plot(history.history['loss'], label='Training Loss')s
+plt.title('Training Loss Over Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(5,5))
+fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.91])
+for i,ax in enumerate(axes.flat):
+    random_index = np.random.randint(m)
+    
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Predict using the Neural Network
+    prediction = model.predict(X[random_index].reshape(1,400))
+    prediction_p = tf.nn.softmax(prediction)
+    yhat = np.argmax(prediction_p)
+    
+    ax.set_title(f"{y[random_index,0]},{yhat}",fontsize=10)
+    ax.set_axis_off()
+fig.suptitle("Label, yhat", fontsize=14)
+plt.show()
+
+n_errors = display_errors(model, X, y)
+print(f"no of errors = {n_errors}")
